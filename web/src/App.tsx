cf8f5d2467d22f52c717c0bdf55cdee7a0a0c8e5
Engin Polat <polatengin@hotmail.com>
2022-05-25 03:54:27 +0000

    adding method to fetch data from backend api

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,6 +2,12 @@ import React from 'react';
 
 export default () => {
 
+  const callApi = async () => {
+    const response = await fetch("https://localhost:3001/api/topic");
+    const body = await response.json();
+    console.log(body);
+  };
+
   return (
     <div>
     </div>
